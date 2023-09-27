1fda1b80cc78b4a76cc0eade4cc6db6b65ae1e5d
surajverma2587 <surajverma2587@gmail.com>
2023-09-27 19:51:20 +0100

    build game grid

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,42 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
+const constructGame = (length) => {
+  const gameDiv = document.createElement("div");
+  gameDiv.setAttribute("class", "jumbotron m-1");
+
+  for (let row = 0; row < length; row++) {
+    // build row
+    const rowDiv = document.createElement("div");
+    rowDiv.setAttribute("class", "row");
+
+    for (let col = 0; col < 3; col++) {
+      // build col
+      const colDiv = document.createElement("div");
+      colDiv.setAttribute("class", "col border text-center p-4 fs-1");
+
+      const icon = document.createElement("i");
+      icon.setAttribute("class", "fa-solid fa-question");
+
+      colDiv.append(icon);
+
+      rowDiv.append(colDiv);
+    }
+
+    gameDiv.append(rowDiv);
+  }
+
+  return gameDiv;
+};
+
 const renderGameContainer = () => {
   const gameContainerDiv = document.createElement("div");
 
-  const gameDiv = document.createElement("div");
   const scoreDiv = document.createElement("div");
   const controlDiv = document.createElement("div");
 
-  gameDiv.textContent = "Game";
   scoreDiv.textContent = "Score";
+
+  const gameDiv = constructGame(3);
 
   controlDiv.setAttribute("class", "jumbotron text-center border m-1");
   const restartBtn = document.createElement("button");
